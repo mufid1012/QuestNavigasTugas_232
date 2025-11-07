857f410ab739dcb7b1c1027bf061d20960259105
mufid1012 <mufidghibran1012@gmail.com>
2025-11-07 15:10:05 +0700

    menambahkan tempat untuk logo

--- app/src/main/java/com/example/questnavigasitugas_232/view/WelcomeScreen.kt
+++ app/src/main/java/com/example/questnavigasitugas_232/view/WelcomeScreen.kt
@@ -1,15 +1,21 @@
 package com.example.questnavigasitugas_232.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
@@ -27,3 +33,12 @@ fun WelcomeScreen(onSubmitClick: () -> Unit) {
             fontWeight = FontWeight.Bold,
             color = Color(0xFF512DA8)
         )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        // logo - di res/drawable
+        Image(
+            painter = painterResource(id = R.drawable.logo_card_lst),
+            contentDescription = "Logo",
+            modifier = Modifier.size(150.dp)
+
