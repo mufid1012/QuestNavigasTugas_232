c3bd69ea7c32ab8381953ceb7e1d777661a44e92
mufid1012 <mufidghibran1012@gmail.com>
2025-11-07 14:58:39 +0700

    menambahkan input nama lengkap dengan tampilan Card di FormulirPendaftaran

--- app/src/main/java/com/example/questnavigasitugas_232/view/FormulirPendaftaran.kt
+++ app/src/main/java/com/example/questnavigasitugas_232/view/FormulirPendaftaran.kt
@@ -1,14 +1,20 @@
 package com.example.questnavigasitugas_232.view
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -57,3 +63,25 @@ fun FormulirPendaftaran(
                 color = Color.White
             )
         }
+
+        Spacer(modifier = Modifier.height(25.dp))
+
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp),
+            shape = RoundedCornerShape(16.dp),
+            elevation = CardDefaults.cardElevation(defaultElevation = 6.dp)
+        ) {
+            Column(
+                modifier = Modifier.padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(15.dp)
+            ) {
+                Text("NAMA LENGKAP", fontWeight = FontWeight.SemiBold)
+                OutlinedTextField(
+                    value = nama,
+                    onValueChange = { nama = it },
+                    singleLine = true,
+                    modifier = Modifier.fillMaxWidth(),
+                    placeholder = { Text("Isian nama lengkap") }
+                )
