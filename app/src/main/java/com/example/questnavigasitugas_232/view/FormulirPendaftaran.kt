9383b9afd01844180aad3505eb08d700bf797f5a
mufid1012 <mufidghibran1012@gmail.com>
2025-11-07 14:57:48 +0700

    menambahkan header dan layout dasar FormulirPendaftaran dengan style ungu

--- app/src/main/java/com/example/questnavigasitugas_232/view/FormulirPendaftaran.kt
+++ app/src/main/java/com/example/questnavigasitugas_232/view/FormulirPendaftaran.kt
@@ -1,11 +1,26 @@
 package com.example.questnavigasitugas_232.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun FormulirPendaftaran(
@@ -19,3 +34,26 @@ fun FormulirPendaftaran(
 
     val listGender = listOf("Laki-laki", "Perempuan")
     val listStatus = listOf("Janda", "Lajang", "Duda")
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFD1C4E9))
+            .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .background(Color(0xFF7E57C2))
+                .padding(start = 24.dp, bottom = 10.dp),
+            contentAlignment = Alignment.BottomStart
+        ) {
+            Text(
+                text = "Formulir Pendaftaran",
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Normal,
+                color = Color.White
+            )
+        }
