e363f8f9faaf94d767b1b20080953fa96310a766
mufid1012 <mufidghibran1012@gmail.com>
2025-11-07 14:52:02 +0700

    menambahkan state input pengguna (nama, jenis kelamin, alamat, status) di DataApp

--- app/src/main/java/com/example/questnavigasitugas_232/Navigasi.kt
+++ app/src/main/java/com/example/questnavigasitugas_232/Navigasi.kt
@@ -1,8 +1,24 @@
 package com.example.questnavigasitugas_232
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
 enum class Navigasi {
     Welcome,
     Detail,
     Formulir
 }
 
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController()
+) {
+    var nama by remember { mutableStateOf("") }
+    var jenisKelamin by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var status by remember { mutableStateOf("") }
