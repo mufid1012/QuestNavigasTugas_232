9e1c1463bb2e36a0aca792c2a5afb87b600b6e6f
mufid1012 <mufidghibran1012@gmail.com>
2025-11-07 15:03:08 +0700

    menambahkan Scaffold dan TopAppBar di TampilData

--- app/src/main/java/com/example/questnavigasitugas_232/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasitugas_232/view/TampilData.kt
@@ -1,7 +1,20 @@
 package com.example.questnavigasitugas_232.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
 
@@ -21,3 +34,23 @@ fun TampilData(
         Pair("STATUS PERKAWINAN", status),
         Pair(stringResource(id = R.string.alamat), alamat)
     )
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.tampil), color = Color.White) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = Color(0xFF7E57C2) // warna ungu tua sama seperti header Formulir
+                )
+            )
+        }
+    ) { isiRuang ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0xFFD1C4E9)) // warna latar sama seperti Formulir
+                .padding(isiRuang)
+                .padding(all = dimensionResource(id = R.dimen.padding_medium)),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(space = dimensionResource(id = R.dimen.padding_small))
+        ) {
