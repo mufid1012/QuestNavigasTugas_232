cd165d0ea14c2ddd3b83d6c0ed5f7f89992f6006
mufid1012 <mufidghibran1012@gmail.com>
2025-11-07 14:52:34 +0700

    menambahkan navigasi dari WelcomeScreen ke Detail di DataApp

--- app/src/main/java/com/example/questnavigasitugas_232/Navigasi.kt
+++ app/src/main/java/com/example/questnavigasitugas_232/Navigasi.kt
@@ -1,11 +1,16 @@
 package com.example.questnavigasitugas_232
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
 enum class Navigasi {
@@ -22,3 +27,17 @@ fun DataApp(
     var jenisKelamin by remember { mutableStateOf("") }
     var alamat by remember { mutableStateOf("") }
     var status by remember { mutableStateOf("") }
+
+    Scaffold { isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = com.example.testnavigasi3layer.Navigasi.Welcome.name,
+            modifier = Modifier.padding(isiRuang)
+        ) {
+            composable(com.example.testnavigasi3layer.Navigasi.Welcome.name) {
+                WelcomeScreen(
+                    onSubmitClick = {
+                        navController.navigate(com.example.testnavigasi3layer.Navigasi.Detail.name)
+                    }
+                )
+            }
